79f7df23da6e3eca20220dfb21125c5db26b7400
wangliang <wangliangpeking@gmail.com>
2015-04-02 13:25:58 +0800

    Add two possible feature

---
+++ src/edu/pku/emotion/feat/LexicalFeatureExtractor.java
@@ -0,0 +1,21 @@
+package edu.pku.emotion.feat;
+
+import java.util.List;
+
+import edu.pku.instance.Weibo;
+
+/**
+ * 
+ * @author intfloat@pku.edu.cn
+ * @author labyrinth@pku.edu.cn
+ *
+ */
+public class LexicalFeatureExtractor implements FeatureExtractorInterface {
+
+    @Override
+    public void extract(Weibo weibo, List<Feature> features) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
