63740c584be57ce2baa25562828bb1f71421ab5c
wangliang <wangliangpeking@gmail.com>
2015-04-09 23:55:03 +0800

    Change mincount & delete duplicate data

--- scripts/gendata.sh
+++ scripts/gendata.sh
@@ -7,13 +7,13 @@
 TRAIN_DATA=../data/train_class.txt
 TEST_DATA=../data/test_class.txt
 # generate opinion classification data
-cat $TRAIN_DATA | grep SOID | cut -d' ' -f2- > train_opinion.txt
+cat $TRAIN_DATA | grep SOID | cut -d' ' -f2- | uniq > train_opinion.txt
 cat $TEST_DATA | grep SOID | cut -d' ' -f2-  > test_opinion.txt
 
 # generate weibo level emotion classification data
-cat $TRAIN_DATA | grep WPID | cut -d' ' -f2-  > train_weibo.txt
+cat $TRAIN_DATA | grep WPID | cut -d' ' -f2-  | uniq > train_weibo.txt
 cat $TEST_DATA | grep WPID | cut -d' ' -f2-  > test_weibo.txt
 
 # generate sentence level emotion classification data
-cat $TRAIN_DATA | grep SPID | cut -d' ' -f2-  > train_sentence.txt
+cat $TRAIN_DATA | grep SPID | cut -d' ' -f2-  | uniq > train_sentence.txt
 cat $TEST_DATA | grep SPID | cut -d' ' -f2-  > test_sentence.txt
